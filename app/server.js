1b068f4b16edb3d52de52d29d3e21fdb7b14d8c1
jmendoza93 <jeanmendoza945@gmail.com>
2026-09-22 22:48:33 -0500

    Add middlware in app

--- app/server.js
+++ app/server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  res.on('finish', () => {
+    console.log(`${new Date().toISOString()} ${req.method} ${req.originalUrl} ${res.statusCode} ${Date.now() - start}ms`);
+  });
+  next();
+});
+
 app.use(express.static('public'));
 
 //Opcional: redirigir raíz a public/index.html
